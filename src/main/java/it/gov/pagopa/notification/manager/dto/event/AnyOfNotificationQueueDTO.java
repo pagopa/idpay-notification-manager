4eeda81e96b197586a7ae42093671d3011b60dd0
Benedetta-fabbri <Benedetta11.Fabbri@emea.nttdata.com>
2023-04-17 17:54:29 +0200

    [IDP-592] added suspension notification

--- src/main/java/it/gov/pagopa/notification/manager/dto/event/AnyOfNotificationQueueDTO.java
+++ src/main/java/it/gov/pagopa/notification/manager/dto/event/AnyOfNotificationQueueDTO.java
@@ -3,9 +3,7 @@ package it.gov.pagopa.notification.manager.dto.event;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-import static it.gov.pagopa.notification.manager.constants.NotificationConstants.AnyNotificationConsumer.SubTypes.ALLOWED_CITIZEN_PUBLISH;
-import static it.gov.pagopa.notification.manager.constants.NotificationConstants.AnyNotificationConsumer.SubTypes.CHECKIBAN_KO;
-import static it.gov.pagopa.notification.manager.constants.NotificationConstants.AnyNotificationConsumer.SubTypes.REFUND;
+import static it.gov.pagopa.notification.manager.constants.NotificationConstants.AnyNotificationConsumer.SubTypes.*;
 
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
@@ -15,7 +13,8 @@ import static it.gov.pagopa.notification.manager.constants.NotificationConstants
 @JsonSubTypes({
         @JsonSubTypes.Type(value = NotificationCitizenOnQueueDTO.class, name = ALLOWED_CITIZEN_PUBLISH),
         @JsonSubTypes.Type(value = NotificationRefundQueueDTO.class, name = REFUND),
-        @JsonSubTypes.Type(value = NotificationIbanQueueDTO.class, name = CHECKIBAN_KO)
+        @JsonSubTypes.Type(value = NotificationIbanQueueDTO.class, name = CHECKIBAN_KO),
+        @JsonSubTypes.Type(value = NotificationSuspensionQueueDTO.class, name = SUSPENSION)
 })
 public interface AnyOfNotificationQueueDTO {
 }
