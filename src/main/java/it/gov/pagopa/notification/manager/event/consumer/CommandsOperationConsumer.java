05dcca4bf3f51b3d83659fcfafcc46d33de8aecb
GiovanaSolorzano <105281122+GiovanaSolorzano@users.noreply.github.com>
2023-08-11 09:44:11 +0200

    feat: Idp 1456 delete initiative notification (#71)
    * IDP-1456 Initializing delete notification
    
    * IDP-1456 refactor audit logs
    
    * IDP-1456 update values.yml
    
    * IDP-1456 update CommandOperationQueueDTO
    
    * IDP-1456 added values.yaml properties
    
    * IDP-1456 change application properties
    
    * IDP-1456 added logback-spring.xml
    
    * IDP-1456 added janino dependency for logback
    
    * IDP-1456 fix consumer config
    
    * IDP-1456 fix consumers config
    
    * IDP-1456 added values properties
    
    * IDP-1456 fix values properties
    
    * IDP-1456 fix clase name
    
    * IDP-1456 fix values
    
    * IDP-1456 fix values
    
    * Revert "IDP-1456 fix values"
    
    This reverts commit 9a40a21fceaff952b6c10ffb61e2f8b7fbbf3fd7.
    
    * IDP-1456 fix check
    
    * IDP-1456 fix test
    
    * IDP-1456 fix service
    
    * IDP-1456 fix CommandOperationQueueDTO
    
    * IDP-1456 fix CommandOperationQueueDTO
    
    * IDP-1456 test
    
    * IDP-1456 clean comments
    
    ---------
    
    Co-authored-by: GiovanaSolorzano <gisolorz@emeal.nttdata.com>

---
+++ src/main/java/it/gov/pagopa/notification/manager/event/consumer/CommandsOperationConsumer.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.notification.manager.event.consumer;
+
+import it.gov.pagopa.notification.manager.dto.event.CommandOperationQueueDTO;
+import it.gov.pagopa.notification.manager.service.NotificationManagerService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class CommandsOperationConsumer {
+
+  @Bean
+  public Consumer<CommandOperationQueueDTO> commandsConsumer(
+          NotificationManagerService notificationManagerService) {
+    return notificationManagerService::processNotification;
+  }
+
+}
