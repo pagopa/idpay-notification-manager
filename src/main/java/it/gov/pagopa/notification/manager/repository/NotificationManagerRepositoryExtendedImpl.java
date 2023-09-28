1668313f29d4f29dc08317dcabd272b04a37c77c
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-09-28 11:52:52 +0200

    [IDP-1857] fix annotation

--- src/main/java/it/gov/pagopa/notification/manager/repository/NotificationManagerRepositoryExtendedImpl.java
+++ src/main/java/it/gov/pagopa/notification/manager/repository/NotificationManagerRepositoryExtendedImpl.java
@@ -2,14 +2,18 @@ package it.gov.pagopa.notification.manager.repository;
 
 import it.gov.pagopa.notification.manager.model.Notification;
 import it.gov.pagopa.notification.manager.model.Notification.Fields;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@Slf4j
 public class NotificationManagerRepositoryExtendedImpl implements NotificationManagerRepositoryExtended{
     private final MongoTemplate mongoTemplate;
 
