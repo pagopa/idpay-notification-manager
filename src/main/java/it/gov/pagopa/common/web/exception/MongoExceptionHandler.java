13b8b0886deef02e9cef8514124eb9be0d479423
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2023-12-20 17:38:02 +0100

    IDP-2222-handling-http-erros

--- src/main/java/it/gov/pagopa/common/web/exception/MongoExceptionHandler.java
+++ src/main/java/it/gov/pagopa/common/web/exception/MongoExceptionHandler.java
@@ -3,7 +3,6 @@ package it.gov.pagopa.common.web.exception;
 import it.gov.pagopa.common.mongo.retry.MongoRequestRateTooLargeRetryer;
 import it.gov.pagopa.common.mongo.retry.exception.MongoRequestRateTooLargeRetryExpiredException;
 import it.gov.pagopa.common.web.dto.ErrorDTO;
-import it.gov.pagopa.notification.manager.constants.NotificationConstants;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
@@ -14,8 +13,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,8 +26,13 @@ public class MongoExceptionHandler {
 
   private final ErrorManager errorManager;
 
-  public MongoExceptionHandler(ErrorManager errorManager) {
+  private final ErrorDTO tooManyRequestsErrorDTO;
+
+  public MongoExceptionHandler(ErrorManager errorManager, @Nullable ErrorDTO tooManyRequestsErrorDTO) {
     this.errorManager = errorManager;
+
+    this.tooManyRequestsErrorDTO = Optional.ofNullable(tooManyRequestsErrorDTO)
+            .orElse(new ErrorDTO("TOO_MANY_REQUESTS", "Too Many Requests"));
   }
 
   @ExceptionHandler(DataAccessException.class)
@@ -48,8 +55,7 @@ public class MongoExceptionHandler {
     return getErrorDTOResponseEntity(ex, request, ex.getRetryAfterMs());
   }
 
-  private ResponseEntity<ErrorDTO> getErrorDTOResponseEntity(Exception ex,
-                                                             HttpServletRequest request, Long retryAfterMs) {
+  private ResponseEntity<ErrorDTO> getErrorDTOResponseEntity(Exception ex, HttpServletRequest request, Long retryAfterMs) {
     String message = ex.getMessage();
 
     log.info(
@@ -67,7 +73,7 @@ public class MongoExceptionHandler {
     }
 
     return bodyBuilder
-            .body(new ErrorDTO(NotificationConstants.ExceptionCode.TOO_MANY_REQUESTS, "TOO_MANY_REQUESTS"));
+            .body(tooManyRequestsErrorDTO);
   }
 
 }
