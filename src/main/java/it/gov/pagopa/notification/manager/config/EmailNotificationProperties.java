f38e4d6d7d829223c37c5fd6c7d2d40d3d617840
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
2025-10-03 12:45:14 +0200

    [UPBE-206] change setters

--- src/main/java/it/gov/pagopa/notification/manager/config/EmailNotificationProperties.java
+++ src/main/java/it/gov/pagopa/notification/manager/config/EmailNotificationProperties.java
@@ -1,16 +1,19 @@
 package it.gov.pagopa.notification.manager.config;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties( prefix = "rest-client.notification.email-notification")
-@Data
+@Getter
+@Setter
 public class EmailNotificationProperties {
     private Subject subject;
 
-    @Data
+    @Getter
+    @Setter
     public static class Subject {
         private String ok;
         private String partial;
