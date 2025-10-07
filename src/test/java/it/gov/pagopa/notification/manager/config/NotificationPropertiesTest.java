b738f9275d3efa9e98bcc319bbfd1089a999c002
Aledelga <alessandro.delgaudio@nttdata.com>
2025-10-07 14:28:11 +0200

    feat: Upbe 208 io comunicazione esito controlli ko (#206)
    Co-authored-by: andreaV95 <157486351+Vitolo-Andrea@users.noreply.github.com>
    Co-authored-by: angelosca24 <angelo.scagliola@nttdata.com>
    Co-authored-by: stedelia <144045955+stedelia@users.noreply.github.com>
    Co-authored-by: GiovanaSolorzano <giovanaelvira.solorzanochirieleison@emeal.nttdata.com>
    Co-authored-by: del Gaudio, Alessandro <alessndro.delgaudio@nttdata.com>

--- src/test/java/it/gov/pagopa/notification/manager/config/NotificationPropertiesTest.java
+++ src/test/java/it/gov/pagopa/notification/manager/config/NotificationPropertiesTest.java
@@ -15,7 +15,13 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
         "notification.manager.markdown.ok-bel=MARKDOWN_OK",
         "notification.manager.markdown.ok-partial-bel=MARKDOWN_OK_PARTIAL",
         "notification.manager.subject.ok-bel=SUBJECT_OK",
-        "notification.manager.subject.ok-partial-bel=SUBJECT_OK_PARTIAL"
+        "notification.manager.subject.ok-partial-bel=SUBJECT_OK_PARTIAL",
+        "notification.manager.subject.joined-bel=SUBJECT_JOINED_BEL",
+        "notification.manager.subject.ko-thanks-bel=SUBJECT_KO_THANKS_BEL",
+        "notification.manager.subject.ko-generic-bel=SUBJECT_KO_GENERIC_BEL",
+        "notification.manager.markdown.joined-bel=MARKDOWN_JOINED_BEL",
+        "notification.manager.markdown.ko-thanks-bel=MARKDOWN_KO_THANKS_BEL",
+        "notification.manager.markdown.ko-generic-bel=MARKDOWN_KO_GENERIC_BEL"
 })
 @EnableConfigurationProperties(value = NotificationProperties.class)
 class NotificationPropertiesTest {
@@ -34,6 +40,15 @@ class NotificationPropertiesTest {
         Assertions.assertNotNull(properties.getMarkdown().getDoubleNewLine());
         Assertions.assertEquals("MARKDOWN_OK", properties.getMarkdown().getOkBel());
         Assertions.assertEquals("MARKDOWN_OK_PARTIAL", properties.getMarkdown().getOkPartialBel());
+
+        Assertions.assertEquals("SUBJECT_KO_THANKS_BEL", properties.getSubject().getKoThanksBel());
+        Assertions.assertEquals("SUBJECT_KO_GENERIC_BEL", properties.getSubject().getKoGenericBel());
+        Assertions.assertEquals("SUBJECT_JOINED_BEL", properties.getSubject().getJoinedBel());
+
+        Assertions.assertEquals("MARKDOWN_JOINED_BEL", properties.getMarkdown().getJoinedBel());
+        Assertions.assertEquals("MARKDOWN_KO_THANKS_BEL", properties.getMarkdown().getKoThanksBel());
+        Assertions.assertEquals("MARKDOWN_KO_GENERIC_BEL", properties.getMarkdown().getKoGenericBel());
+
 
 
     }
