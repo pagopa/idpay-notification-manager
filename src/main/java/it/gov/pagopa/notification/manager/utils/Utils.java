a4bd3ed9087a1fb85bd1024d9e658bffd3788f3c
del Gaudio, Alessandro <alessndro.delgaudio@nttdata.com>
2025-10-08 16:32:18 +0200

    UPBE-252 added junit test

--- src/main/java/it/gov/pagopa/notification/manager/utils/Utils.java
+++ src/main/java/it/gov/pagopa/notification/manager/utils/Utils.java
@@ -1,18 +1,17 @@
 package it.gov.pagopa.notification.manager.utils;
 
 import it.gov.pagopa.notification.manager.dto.PersonNameable;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
-@NoArgsConstructor
 public class Utils {
 
     @NotNull
     public static <T extends PersonNameable> String getNameSurname(T dto) {
         return dto.getName() + " " + dto.getSurname();
+    }
+
+    private Utils() {
     }
 }
