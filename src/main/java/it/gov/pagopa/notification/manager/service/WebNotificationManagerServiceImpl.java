e8b495c5c67d2cef1846974304f4a79cde0b108a
EMELIGMWW <marta.meligarziera@emeal.nttdata.com>
2025-11-02 23:45:02 +0100

    fix subject

--- src/main/java/it/gov/pagopa/notification/manager/service/WebNotificationManagerServiceImpl.java
+++ src/main/java/it/gov/pagopa/notification/manager/service/WebNotificationManagerServiceImpl.java
@@ -11,6 +11,7 @@ import it.gov.pagopa.notification.manager.model.Notification;
 import it.gov.pagopa.notification.manager.repository.NotificationManagerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -47,17 +48,24 @@ public class WebNotificationManagerServiceImpl implements  WebNotificationManage
         Map<String, String> templateValues = new HashMap<>();
         templateValues.put("name", notificationQueueDTO.getName());
         templateValues.put("voucherEndDate", notificationQueueDTO.getVoucherEndDate().format(formatter));
-        templateValues.put("expiringDay",String.valueOf(notificationQueueDTO.getExpiringDay()));
 
         EmailMessageDTO emailMessageDTO = EmailMessageDTO.builder()
                 .templateName(EMAIL_OUTCOME_THREE_DAY_REMINDER)
                 .recipientEmail(notificationQueueDTO.getUserMail())
                 .senderEmail(null)
                 .templateValues(templateValues)
-                .subject(emailNotificationProperties.getSubject().getOkThreeDayReminder())
+                .subject(replaceMessageItem(emailNotificationProperties.getSubject().getOkThreeDayReminder(),
+                        NotificationConstants.EXPIRING_DAY_KEY,
+                        String.valueOf(notificationQueueDTO.getExpiringDay())))
                 .content(null)
                 .build();
         sendNotification(emailMessageDTO, notificationQueueDTO);
+    }
+
+    private String replaceMessageItem(String message, String key, String value) {
+        return message.replace(
+                NotificationConstants.MARKDOWN_TAG + key + NotificationConstants.MARKDOWN_TAG,
+                StringUtils.hasLength(value) ? value : NotificationConstants.MARKDOWN_NA);
     }
 
     void sendNotification(EmailMessageDTO notificationToSend, NotificationReminderQueueDTO notificationQueueDTO) {
